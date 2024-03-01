3fae357184ed5d1d2b2bf859b9aea276c0653aec
Daniel-MSoares <munizsoaresrapper@gmail.com>
2024-02-29 21:53:24 -0300

    init footer and update readme

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
+At the first time, run this command to install the project dependencies :
 
-First, run the development server:
+```bash
+npm install
+
+```
+
+After, run the development server:
 
 ```bash
 npm run dev
--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,7 +1,26 @@
+'use client';
+import { Icon } from 'semantic-ui-react';
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
 export function Footer() {
   return (
-    <footer>
-      <span>footer</span>
+    <footer className="w-full bg-sky-950 min-h-12 mt-1 p-6">
+      <div>
+        <button
+          onClick={scrollToTop}
+          className="flex border-2 border-blue-200 w-10 h-10 justify-center rounded-md p-1 "
+        >
+          <Icon
+            name="angle double up"
+            size="large"
+            fitted
+            className="text-blue-200 inline-block text-xl align-middle "
+          />
+        </button>
+      </div>
+      <div></div>
     </footer>
   );
 }
