623410d58cf0f8edbc2e3e48ed7e9aaf47706339
Daniel-MSoares <munizsoaresrapper@gmail.com>
2024-02-29 23:12:40 -0300

    footer

--- src/app/components/smallComponents/sectionSeparator.jsx
+++ src/app/components/smallComponents/sectionSeparator.jsx
@@ -1,7 +1,7 @@
 export function SectionSeparator({ title }) {
   return (
-    <div className="flex w-full h-48 border items-center bg-fixed bg-parallax-effect bg-cover">
-      <div className="flex w-full justify-center items-center bg-blue-600/50 h-full">
+    <div className="flex w-full h-48 border items-center bg-fixed bg-parallax-effect bg-cover ">
+      <div className="flex w-full justify-center items-center bg-blue-600/50 h-full animate-slidein [--slidein-delay:900ms] opacity-0">
         <h2 className="text-4xl md:text-5xl  text-balance leading-relaxed uppercase text-white font-bold text-center">
           {title}
         </h2>
